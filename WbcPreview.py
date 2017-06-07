69c679f6abd4ea6370ca2ba9f1958d8bd09982a8
W. Craig Trader <craig@trader.name>
2017-06-07 02:12:03 -0400

    Update copyright dates

--- WbcPreview.py
+++ WbcPreview.py
@@ -1,4 +1,4 @@
-# ----- Copyright (c) 2010-2016 by W. Craig Trader ---------------------------------
+# ----- Copyright (c) 2010-2017 by W. Craig Trader ---------------------------------
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published
